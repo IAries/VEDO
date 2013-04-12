a9f9dcb772d4df2133a81a6b9f35196380b955c6
Wei-Tze Chang <aries@iaries.net>
2013-04-12 13:20:34 +0800

    Update the class "TimeHistory"

--- src/NJR/Interfaces/TimeHistory.h
+++ src/NJR/Interfaces/TimeHistory.h
@@ -1,6 +1,7 @@
 #ifndef _NJR_TIME_HISTORY_H
 #define _NJR_TIME_HISTORY_H
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,27 +9,140 @@
 namespace njr
 {
 
+template <typename T>
 class TimeHistory
 {
 
 public:
 
-	TimeHistory(const char* cFileName);
+	TimeHistory()
+	{
+		_sFileName = "No Name";
+	};
 
-	TimeHistory(const TimeHistory&);
+	TimeHistory(const char* cFileName)
+	{
+		_sFileName = cFileName;
+		double dTime;
+		T      tValue;
+		std::ifstream iFile(cFileName, std::ios::in);
 
-	const TimeHistory& operator = (const TimeHistory&);
+		// Count the size of time history
+		unsigned long ulNumData = 0;
+		std::string sTempLine;
+		while(getline(iFile, sTempLine))
+		{
+			if(sTempLine.size() != 0)
+				ulNumData++;
+		}
 
-	~TimeHistory();
+		iFile.clear();
+		iFile.seekg(0, std::ios::beg);
+		for (unsigned u=0; u<ulNumData; u++)
+		{
+			iFile >> dTime >> tValue;
+			_vdtTimeAndValue.push_back(std::make_pair(dTime, tValue));
+		}
+		iFile.close();
+	};
+
+	TimeHistory(const TimeHistory& t)
+	{
+		*this = t;
+	};
+
+	const TimeHistory& operator = (const TimeHistory& t)
+	{
+		_sFileName = t._sFileName;
+		_vdtTimeAndValue.clear();
+		for (unsigned u=0; u<t._vdtTimeAndValue.size(); u++)
+			_vdtTimeAndValue.push_back(t._vdtTimeAndValue[u]);
+
+		return *this;
+	};
+
+	~TimeHistory()
+	{
+	};
 
 	inline unsigned GetNumHistory() const
 	{
-		return _vddTimeAndValue.size();
+		return _vdtTimeAndValue.size();
 	};
 
-	std::pair<double, double> GetHistory(unsigned uSerialNumber) const;
+	std::pair<double, T> GetHistory(unsigned uSerialNumber) const
+	{
+		if (uSerialNumber < _vdtTimeAndValue.size())
+		{
+			return _vdtTimeAndValue[uSerialNumber];
+		}
+		else
+		{
+			return std::make_pair(0.0, T());
+		}
+	};
 
-	double GetValue(double dTime) const;
+	T AddValue(double dTime, T tValue)
+	{
+		_vdtTimeAndValue.push_back(std::make_pair(dTime, tValue));
+	};
+
+	T GetValue(double dTime) const
+	{
+		if (_vdtTimeAndValue.size() == 0)
+			return 0.0;
+
+		if (_vdtTimeAndValue.size() == 1)
+			return _vdtTimeAndValue[0].second;
+
+		double tValue;
+		double dTime1, dTime2, tValue1, tValue2;
+
+		if (dTime <= (_vdtTimeAndValue[0].first))
+		{
+			dTime1  = _vdtTimeAndValue[0].first;
+			tValue1 = _vdtTimeAndValue[0].second;
+			dTime2  = _vdtTimeAndValue[1].first;
+			tValue2 = _vdtTimeAndValue[1].second;
+			tValue
+				= tValue1
+				- (dTime1 - dTime) * (tValue2 - tValue1) / (dTime2 - dTime1);
+		}
+		else
+		{
+			bool bNotFound = true;
+			unsigned u = 0;
+			while (bNotFound)
+			{
+				if (dTime < (_vdtTimeAndValue[u].first))
+				{
+					bNotFound = false;
+					dTime2    = (_vdtTimeAndValue[u].first);
+					tValue2   = (_vdtTimeAndValue[u].second);
+					dTime1    = (_vdtTimeAndValue[u-1].first);
+					tValue1   = (_vdtTimeAndValue[u-1].second);
+					tValue
+						= tValue1
+						+ (dTime - dTime1) * (tValue2 - tValue1) / (dTime2 - dTime1);
+				}
+				u++;
+				if (u == _vdtTimeAndValue.size())
+				{
+					u--;
+					dTime2  = (_vdtTimeAndValue[u].first);
+					tValue2 = (_vdtTimeAndValue[u].second);
+					dTime1  = (_vdtTimeAndValue[u-1].first);
+					tValue1 = (_vdtTimeAndValue[u-1].second);
+					tValue
+						= tValue2
+						+ (dTime - dTime2) * (tValue2 - tValue1) / (dTime2 - dTime1);
+					bNotFound = false;
+				}
+			}
+		}
+
+		return tValue;
+	};
 
 	inline std::string GetFileName() const
 	{
@@ -37,15 +151,24 @@ public:
 
 protected:
 
-	TimeHistory();
-
 	std::string _sFileName;
 
-	std::vector<std::pair<double, double> > _vddTimeAndValue;
+	std::vector<std::pair<double, T> > _vdtTimeAndValue;
 };
 
 };   // namespace njr
 
-std::ostream& operator << (std::ostream& os, const njr::TimeHistory& th);
+
+
+template <typename T>
+std::ostream& operator << (std::ostream& os, const njr::TimeHistory<T>& t)
+{
+	os << "File name: " << t.GetFileName() << std::endl;
+	for (unsigned u=0; u<t.GetNumHistory(); u++)
+	{
+		os << t.GetHistory(u).first << ", " << t.GetHistory(u).second << std::endl;
+	}
+	return os;
+};
 
 #endif // _NJR_TIME_HISTORY_H
